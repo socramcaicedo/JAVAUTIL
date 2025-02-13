a01525d2e5411e97ce007a1a03bf54321e2932a7
APRENDIZ <APRENDIZ@STMAPRRMDFSP609.FormacionSena.RED>
2025-02-12 19:02:01 -0500

    saber que ando haciendo

--- programas/src/programas/hola.java
+++ programas/src/programas/hola.java
@@ -1,9 +1,14 @@
 package programas;
 
+import java.util.Scanner;
+
 public class hola {
 
 	public static void main(String[] args) {
+		Scanner lt =new Scanner(System.in);
 		System.out.println("CAICEDO ESTAS AQUI");
+		var nombre ="";
+		nombre = lt.next();
 	}
 
 }
